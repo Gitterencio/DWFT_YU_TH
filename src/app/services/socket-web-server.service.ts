3b32f993b803e9a2791240c39c8d78ddad6ef9bc
alberto mata <terenciomatamoros@gmail.com>
2023-04-10 09:53:04 -0600

    test

--- src/app/services/socket-web-server.service.ts
+++ src/app/services/socket-web-server.service.ts
@@ -16,7 +16,7 @@ export class SocketWebServerService {
   devURL:string = `http://${this.hostname}:3000`;
   productURL:string = `https://dwbkyuth-production.up.railway.app`;
 
-  server_url:string= this.devURL;
+  server_url:string= this.productURL;
 
   io = io(this.server_url,{
     withCredentials:true,
@@ -27,19 +27,28 @@ export class SocketWebServerService {
   constructor() { 
 
     this.connection();
+<<<<<<< HEAD
     this.OnJoinedRoomProject();
     this.OnsocketNewClient();
 
     this.onEditedHTMLProject();
+=======
+    this.OnsocketNewClient();
+>>>>>>> ff2b915 (services/integration/testconexion)
   }
 
   connection(){
 
+<<<<<<< HEAD
     this.io.io.opts.query = {message:"CONNECTION SUCCESS"}
+=======
+    this.io.io.opts.query = {room:"CONEXION REALIZADA CON EXITO"}
+>>>>>>> ff2b915 (services/integration/testconexion)
     this.io.connect();
 
   }
 
+<<<<<<< HEAD
   joinRoomProject(idRoom:string){
     this.io.emit("JoinRoomProject",idRoom);
   }
@@ -55,6 +64,8 @@ export class SocketWebServerService {
     this.io.on("EditedHTMLProject", res => this.htmlResponse.emit(res));
   }
 
+=======
+>>>>>>> ff2b915 (services/integration/testconexion)
   _socketNewApplicationClient(){
     this.io.emit("NewApplicationClient");
   }
