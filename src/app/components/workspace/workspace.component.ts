28339c056d1e0908a3274789b7f86487f381bed9
albertmata <albertmata@pop-os.localdomain>
2023-05-03 20:23:29 -0600

    preview

--- src/app/components/workspace/workspace.component.ts
+++ src/app/components/workspace/workspace.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./workspace.component.css']
 })
 export class WorkspaceComponent {
+  html_edit:string = `<h1>Hola mundo</h1>`;
+  css_edit:string = `h1{color:red;}`;
+  js_edit:string = `console.log('hola mundo')`;
 
+  preview:string =`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <style>
+        ${this.css_edit}
+      </style>
+    </head>
+    <body>
+      <script>
+        ${this.js_edit}
+      </script>
+      ${this.html_edit}
+    </body>
+  </html>
+
+  `
+
+
+  ngOnInit(): void {
+    this.inicializarPreview();
+  }
+
+  inicializarPreview(){
+    document.getElementById('preview')?.setAttribute('srcdoc',this.preview);
+  }
+  onChangeCode(){
+    this.preview =`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <style>
+          ${this.css_edit}
+        </style>
+      </head>
+      <body>
+        <script>
+          ${this.js_edit}
+        </script>
+        ${this.html_edit}
+      </body>
+    </html>
+  
+    `
+    document.getElementById('preview')?.setAttribute('srcdoc',this.preview);
+  }
+  
 }
